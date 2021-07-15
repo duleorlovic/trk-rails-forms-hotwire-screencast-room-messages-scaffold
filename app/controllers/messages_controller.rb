74f8acdd59ad94d576ea877cdf5f7436fd1e61e2
Dusan Orlovic <duleorlovic@gmail.com>
2021-07-15 19:03:22 +0200

    Add message partial and form, standard rails staff

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,30 @@
+class MessagesController < ApplicationController
+  before_action :set_room, only: %i[ new create ]
+
+  def new
+    @message = @room.messages.new
+  end
+
+  def create
+    @message = @room.messages.new(message_params)
+
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to @room, notice: 'message was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_room
+      @room = Room.find(params[:room_id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def message_params
+      params.require(:message).permit(:content)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
+  root to: 'rooms#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
